9cdacf53530d0c2bed25b71f7ff61ad636a519d2
stalingeorg <stalingeorge46@gmail.com>
2022-11-03 10:26:03 +0530

    multi

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,11 @@
 import React  from 'react';
 import './index.css';
 import Banner from './components/Banner';
-import Section1 from './components/Section1';
   
 const Home =() =>{
   return(
     <>
     <Banner/>
-    <Section1/>
     </>
   );
 };
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,7 +6,7 @@ import logoImg from './assets/images/logo.svg';
 const navbaritems = [  
   {  
     'id': 1,   
-    'link': '/home',   
+    'link': '/ ',   
     'name': 'Home'  
   },  
   {   
@@ -19,13 +19,13 @@ const Navbar =() =>{
   
   return(
     <>
-    <nav className="navbar navbar-expand-lg" style={{position:"fixed",width: "100%",backgroundColor: "transparent"}}>
+    <nav className="navbar navbar-dark navbar-expand-lg" style={{position:"fixed",width: "100%",backgroundColor: "transparent"}}>
   <div className="container-fluid">
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <NavLink className="navbar-brand" to="/home">
+      <NavLink className="navbar-brand" to="/ ">
         <img src={logoImg} alt="" style={{width:"123px",height:"123px"}} />
       </NavLink>
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0 pe-lg-5">
